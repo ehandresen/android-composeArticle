7b959ba9bbe852675d781378d38437c0794a1c02
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-12-19 16:07:48 +0100

    Compose quadrant screen

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -37,7 +39,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeArticleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TaskDone(stringResource(R.string.text1), stringResource(R.string.text2))
+                    Column {
+                        Row {
+                            TextComposable()
+                            ImageComposable()
+                        }
+                        Row {
+                            RowComposable()
+                            ColumnComposable()
+                        }
+                    }
                 }
             }
         }
@@ -45,29 +56,94 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TaskDone(text1: String, text2: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.ic_task_completed)
-    
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
+fun TextComposable(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier
+        .fillMaxSize(0.5F)
+        .background(Color(0xFFEADDFF))
+        .padding(16.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Image(
-            painter = image,
-            contentDescription = null
-        )
         Text(
-            text = text1,
+            text = "Text composable",
             fontWeight = FontWeight.Bold,
             modifier = Modifier
-                .padding(top = 24.dp, bottom = 8.dp)
+                .padding(bottom = 16.dp)
         )
         Text(
-            text = text2,
-            fontSize = 16.sp
+            text = "Displays text and follows the recommended Material Design guidelines.",
+            textAlign = TextAlign.Justify
+
+        )
+    }
+}
+
+@Composable
+fun ImageComposable(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier
+        .fillMaxHeight(0.5F)
+        .background(Color(0xFFD0BCFF))
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Image composable",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = "Creates a composable that lays out and draws a given Painter class object.",
+            textAlign = TextAlign.Justify
+
+        )
+    }
+}
+
+@Composable
+fun RowComposable(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier
+        .fillMaxHeight(1.0F)
+        .fillMaxWidth(0.5F)
+        .background(Color(0xFFB69DF8))
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Row composable",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = "A layout composable that places its children in a horizontal sequence.",
+            textAlign = TextAlign.Justify
+
+        )
+    }
+}
+
+@Composable
+fun ColumnComposable(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFFF6EDFF))
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Column composable",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+        )
+        Text(
+            text = "A layout composable that places its children in a vertical sequence.",
+            textAlign = TextAlign.Justify
+
         )
     }
 }
@@ -76,7 +152,16 @@ fun TaskDone(text1: String, text2: String, modifier: Modifier = Modifier) {
 @Composable
 fun ArticlePreview() {
     ComposeArticleTheme {
-        TaskDone(stringResource(R.string.text1), stringResource(R.string.text2))
+        Column {
+            Row {
+                TextComposable()
+                ImageComposable()
+            }
+            Row {
+                RowComposable()
+                ColumnComposable()
+            }
+        }
     }
 }
 
