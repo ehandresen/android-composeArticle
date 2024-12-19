aa5d33473a0d787ec49398ec2cb9860d881e55d4
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-12-19 15:18:31 +0100

    task manager screen

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -8,15 +8,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composearticle.ui.theme.ComposeArticleTheme
@@ -33,14 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeArticleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
-                        BannerImage()
-                        ArticleText(
-                            stringResource(R.string.title),
-                            stringResource(R.string.section1),
-                            stringResource(R.string.section2)
-                        )
-                    }
+                    TaskDone(stringResource(R.string.text1), stringResource(R.string.text2))
                 }
             }
         }
@@ -48,55 +45,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BannerImage(modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.bg_compose_background)
-
-    Image(
-        painter = image,
-        contentDescription = null,
-        //contentScale = ContentScale.Crop,
-        //alpha = 0.6F
-    )
-}
-
-@Composable
-fun ArticleText(
-    title: String,
-    section1: String,
-    section2: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = title,
-        fontSize = 24.sp,
+fun TaskDone(text1: String, text2: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    
+    Column(
         modifier = Modifier
-            .padding(16.dp)
-    )
-    Text(
-        text = section1,
-        textAlign = TextAlign.Justify,
-        modifier = Modifier
-            .padding(16.dp)
-    )
-    Text(
-        text = section2,
-        textAlign = TextAlign.Justify,
-        modifier = Modifier
-            .padding(16.dp)
-    )
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = text1,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = text2,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ArticlePreview() {
     ComposeArticleTheme {
-        Column {
-            BannerImage()
-            ArticleText(
-                stringResource(R.string.title),
-                stringResource(R.string.section1),
-                stringResource(R.string.section2)
-            )
-        }
+        TaskDone(stringResource(R.string.text1), stringResource(R.string.text2))
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
